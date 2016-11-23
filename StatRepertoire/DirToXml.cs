1fea489572d04979b2b8291339b1dcfce5d32d6d
jah4ck <johann.simon.web@gmail.com>
2016-11-23 10:00:01 +0100

    my first commit

---
+++ StatRepertoire/DirToXml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Linq;
+
+namespace StatRepertoire
+{
+    public static class DirToXml
+    {
+        
+        public static XElement BuildTree(string dirName)
+
+        {
+
+            var di = new DirectoryInfo(dirName);
+            var files = di.GetFiles();
+            string toto = "";
+            
+            foreach (var file in files)
+
+            {
+                if (file.ToString().Contains(".dll"))
+                {
+                    toto = (file + ";" + dirName + @"\" + file.ToString() + ";" + GetMD5HashFromFile(dirName + @"\" + file.ToString()));
+                }
+                //write.Flush();
+            }
+            
+
+            var subdirs = di.GetDirectories();
+
+            // each item is a "directory" having 5 attributes
+
+            // name is the name of the folder
+
+            // fullpath is the full path including the name of the folder
+
+            // size is the size of all files in the folder (in bytes)
+
+            // files is the number of files in the folder
+
+            // subdirs is the count of possible sub directories in the folder
+            var elem = new XElement(new XElement("directory"));
+            if (toto.Length > 5)
+            {
+                elem = new XElement("directory",
+
+                    new XAttribute("name", toto));
+            }
+
+            foreach (var dinf in subdirs)
+
+            {
+                
+                var elemDir = BuildTree(dirName + "\\" + dinf.Name);
+                //Console.WriteLine(dirName + ";" + dirsize);
+                
+                    elem.Add(elemDir);
+                
+
+            }
+
+ 
+
+            return elem;
+
+        }
+
+        public static string GetMD5HashFromFile(string fileName)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(fileName))
+                {
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty);
+                }
+            }
+        }
+
+    
+    }
+}
---
+++ StatRepertoire/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Xml.Linq;
+using System.Security.Cryptography;
+
+
+namespace StatRepertoire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DirToXml.BuildTree(@"C:\Windows\Microsoft.NET\Framework");
+            StreamWriter write = new StreamWriter(@"C:\TEMP\RESULT_JS\MD5.txt");
+
+            string[] lstFile = Directory.GetFiles(@"C:\Windows\Microsoft.NET", "*", SearchOption.AllDirectories);
+            foreach (string file in lstFile)
+            {
+                try
+                {
+                    string nom = file.Substring(file.LastIndexOf(@"\") + 1);
+                    string md5 = GetMD5HashFromFile(file);
+                    write.WriteLine(nom + ";" + file + @"\" + file.ToString() + ";" + md5);
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine(err.Message);
+                }
+                
+            }
+           // write.WriteLine(DirToXml.BuildTree(@"C:\Windows\Microsoft.NET"));
+            write.Close();
+            Console.WriteLine("done!");
+            Console.ReadLine();
+                        
+        }
+        public static string GetMD5HashFromFile(string fileName)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(fileName))
+                {
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty);
+                }
+            }
+        }
+
+    }
+
+
+        
+    
+}
